bad359015d9730b6cf4337f4a931aa4630168556
Joey Eric <joeyebelisque@gmail.com>
2026-03-16 15:54:49 -0300

    commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import { Heading } from './components/Heading';
-import './styles/global.css/';
-import './styles/theme.css/';
+import './styles/theme.css';
+import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/index.tsx';
 
+export function App() {
+  return (
+    <>
+    <Container>
+        <Heading>Testando meu componente</Heading>
+    </Container>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
 
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
 
-export function App(){
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
 
-    return(
-        <>  
-            <Heading />
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi soluta distinctio nam non quidem facilis veniam incidunt natus doloribus consequuntur iste possimus esse adipisci expedita saepe, beatae magnam est dolorum.
-            </p>
-        </>
-    );
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
+    </>
+  );
 }
-export default App;
